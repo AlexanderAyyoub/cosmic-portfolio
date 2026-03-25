623e393e3b6c00e7a4005fdf96b486f7aa43ab96
AlexanderAyyoub <aa322014@gmail.com>
2026-03-25 01:36:25 -0400

    Update
    Deployment fix

--- app/starPage/[slug]/page.tsx
+++ app/starPage/[slug]/page.tsx
@@ -1,15 +1,14 @@
-
 import checkStar from "@/app/server_actions/checkStarById";
 import getStarById from "@/app/server_actions/getStarById";
 import StarPageScene from "components/star-page-scene";
 import { notFound } from "next/navigation";
 
 type PageProps = {
-    params: { slug: string };
-}
+    params: Promise<{ slug: string }>;
+};
 
 export default async function DynamicStarPage(props: PageProps) {
-    const { slug } = (await props.params);
+    const { slug } = await props.params;
 
     if (!slug || isNaN(Number(slug)) || Number(slug) < 1) {
         return notFound();
@@ -22,7 +21,6 @@ export default async function DynamicStarPage(props: PageProps) {
         return notFound();
     }
 
-    // Fetch the star data
     const starResponse = await getStarById({ starId: slug });
     const { star } = await starResponse.json();
 
@@ -32,7 +30,5 @@ export default async function DynamicStarPage(props: PageProps) {
 
     return (
         <StarPageScene star={star} />
-        
-    ); 
-
+    );
 }
